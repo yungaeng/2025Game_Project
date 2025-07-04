5d1ede30650bc37d887dc7e30ea525b24dd9ceb7
Ku yun song <kys1091201@naver.com>
2025-07-04 23:59:57 +0900

    DB

---
+++ Server/C++ Server/IOCP-Unreal-Server/IOCP-Unreal-Server/CONN.cpp
@@ -0,0 +1,53 @@
+// SQLConnect_ref.cpp  
+// compile with: odbc32.lib  
+#include <windows.h>  
+#include <sqlext.h>  
+#include <iostream>
+
+int main() {
+    SQLHENV henv;
+    SQLHDBC hdbc;
+    SQLHSTMT hstmt;
+    SQLRETURN retcode;
+
+    SQLCHAR* OutConnStr = (SQLCHAR*)malloc(255);
+    SQLSMALLINT* OutConnStrLen = (SQLSMALLINT*)malloc(255);
+
+    // Allocate environment handle  
+    retcode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);
+
+    // Set the ODBC version environment attribute  
+    if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+        retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (void*)SQL_OV_ODBC3, 0);
+
+        // Allocate connection handle  
+        if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+            retcode = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc);
+
+            // Set login timeout to 5 seconds  
+            if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+                SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0);
+
+                // Connect to data source  
+                retcode = SQLConnect(hdbc, (SQLWCHAR*)L"2025_Game", SQL_NTS, (SQLWCHAR*)NULL, 0, NULL, 0);
+
+                // Allocate statement handle  
+                if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+                    retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
+
+                    std::cout << "SUCCESS!\n";
+
+                    // Process data  
+                    if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+                        SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+                    }
+
+                    SQLDisconnect(hdbc);
+                }
+
+                SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+            }
+        }
+        SQLFreeHandle(SQL_HANDLE_ENV, henv);
+    }
+}
---
+++ Server/C++ Server/IOCP-Unreal-Server/IOCP-Unreal-Server/READ.cpp
@@ -0,0 +1,120 @@
+// SQLBindCol_ref.cpp  
+// compile with: odbc32.lib  
+#include <windows.h>  
+#include <stdio.h>  
+
+#include <sqlext.h>  
+#include <locale.h>
+
+#define NAME_LEN 50  
+#define PHONE_LEN 60
+
+void show_error() {
+    printf("error\n");
+}
+
+/************************************************************************
+/* HandleDiagnosticRecord : display error/warning information
+/*
+/* Parameters:
+/*      hHandle     ODBC handle
+/*      hType       Type of handle (SQL_HANDLE_STMT, SQL_HANDLE_ENV, SQL_HANDLE_DBC)
+/*      RetCode     Return code of failing command
+/************************************************************************/
+void HandleDiagnosticRecord(SQLHANDLE hHandle, SQLSMALLINT hType, RETCODE RetCode)
+{
+    SQLSMALLINT iRec = 0;
+    SQLINTEGER  iError;
+    WCHAR       wszMessage[1000];
+    WCHAR       wszState[SQL_SQLSTATE_SIZE + 1];
+
+    if (RetCode == SQL_INVALID_HANDLE) {
+        fwprintf(stderr, L"Invalid handle!\n");
+        return;
+    }
+    while (SQLGetDiagRec(hType, hHandle, ++iRec, wszState, &iError, wszMessage,
+        (SQLSMALLINT)(sizeof(wszMessage) / sizeof(WCHAR)), (SQLSMALLINT*)NULL) == SQL_SUCCESS) {
+        // Hide data truncated..
+        if (wcsncmp(wszState, L"01004", 5)) {
+            fwprintf(stderr, L"[%5.5s] %s (%d)\n", wszState, wszMessage, iError);
+        }
+    }
+}
+
+int main() {
+    SQLHENV henv;
+    SQLHDBC hdbc;
+    SQLHSTMT hstmt = 0;
+    SQLRETURN retcode;
+    SQLWCHAR szName[NAME_LEN];
+    SQLINTEGER user_id, user_level;
+    SQLLEN cbName = 0, cb_user_id = 0, cb_user_level = 0;
+
+    setlocale(LC_ALL, "korean");
+
+
+    // Allocate environment handle  
+    retcode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);
+
+    // Set the ODBC version environment attribute  
+    if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+        retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER*)SQL_OV_ODBC3, 0);
+
+        // Allocate connection handle  
+        if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+            retcode = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc);
+
+            // Set login timeout to 5 seconds  
+            if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+                SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0);
+
+                // Connect to data source  
+                retcode = SQLConnect(hdbc, (SQLWCHAR*)L"2025_game", SQL_NTS, (SQLWCHAR*)NULL, 0, NULL, 0);
+
+                // Allocate statement handle  
+                if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+                    retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
+
+                    retcode = SQLExecDirect(hstmt, (SQLWCHAR*)L"SELECT user_id, user_name, user_level FROM user_table ORDER BY 2, 1, 3", SQL_NTS);
+                    if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+
+                        // Bind columns 1, 2, and 3  
+                        retcode = SQLBindCol(hstmt, 1, SQL_C_LONG, &user_id, 100, &cb_user_id);
+                        retcode = SQLBindCol(hstmt, 2, SQL_C_WCHAR, szName, NAME_LEN, &cbName);
+                        retcode = SQLBindCol(hstmt, 3, SQL_C_LONG, &user_level, PHONE_LEN, &cb_user_level);
+
+                        // Fetch and print each row of data. On an error, display a message and exit.  
+                        for (int i = 0; ; i++) {
+                            retcode = SQLFetch(hstmt);
+                            if (retcode == SQL_ERROR || retcode == SQL_SUCCESS_WITH_INFO)
+                                show_error();
+                            if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO)
+                            {
+                                //replace wprintf with printf
+                                //%S with %ls
+                                //warning C4477: 'wprintf' : format string '%S' requires an argument of type 'char *'
+                                //but variadic argument 2 has type 'SQLWCHAR *'
+                                //wprintf(L"%d: %S %S %S\n", i + 1, sCustID, szName, szPhone);  
+                                printf("%d: %d %ls %d\n", i + 1, user_id, szName, user_level);
+                            }
+                            else
+                                break;
+                        }
+                    }
+                    else HandleDiagnosticRecord(hstmt, SQL_HANDLE_STMT, retcode);
+
+                    // Process data  
+                    if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+                        SQLCancel(hstmt);
+                        SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+                    }
+
+                    SQLDisconnect(hdbc);
+                }
+
+                SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+            }
+        }
+        SQLFreeHandle(SQL_HANDLE_ENV, henv);
+    }
+}
