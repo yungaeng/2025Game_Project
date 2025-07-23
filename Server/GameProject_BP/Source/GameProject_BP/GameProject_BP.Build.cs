50ccb09ab73cb6dfdf12882e6d9db47ee916ce33
Ku yun song <kys1091201@naver.com>
2025-07-24 03:33:56 +0900

    0724

--- Server/GameProject_BP/Source/GameProject_BP/GameProject_BP.Build.cs
+++ Server/GameProject_BP/Source/GameProject_BP/GameProject_BP.Build.cs
@@ -4,20 +4,30 @@ using UnrealBuildTool;
 
 public class GameProject_BP : ModuleRules
 {
-	public GameProject_BP(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AudioCapture", "AudioAnalyzer", "AudioMixer", "Sockets", "Networking", "OnlineSubsystemSteam", "OnlineSubsystem" });
+    public GameProject_BP(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "AudioCapture", "AudioAnalyzer", "AudioMixer" });
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "Core", "CoreUObject", "Engine", "InputCore",
+            "AudioCapture", "AudioAnalyzer", "AudioMixer",
+            "Sockets", "Networking", "OnlineSubsystemSteam", "OnlineSubsystem"
+        });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        PrivateDependencyModuleNames.AddRange(new string[] {
+            "AudioCapture", "AudioAnalyzer", "AudioMixer"
+        });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        if (Target.bBuildEditor)
+        {
+            PrivateDependencyModuleNames.AddRange(new string[] {
+                "UMGEditor",
+                "SequencerCore",
+                "CurveEditor",
+                "PropertyEditor",
+                "EditorConfig",
+                "UnrealEd"
+            });
+        }
+    }
 }
