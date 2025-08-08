b6d768df85e6b374ab1a15fda774fa228bd98727
구윤성 <61838460+yungaeng@users.noreply.github.com>
2025-08-08 17:38:40 +0900

    플러그인 추가

--- Server/multitest/Source/multitest/multitest.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "multitest.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, multitest, "multitest" );
- 
