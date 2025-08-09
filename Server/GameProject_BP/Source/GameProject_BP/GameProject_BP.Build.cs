44e062f4e48fdf7e773fd253f33cf717e36feaae
구윤성 <61838460+yungaeng@users.noreply.github.com>
2025-08-10 03:47:59 +0900

    패키징

--- Server/GameProject_BP/Source/GameProject_BP/GameProject_BP.Build.cs
+++ Server/GameProject_BP/Source/GameProject_BP/GameProject_BP.Build.cs
@@ -8,7 +8,7 @@ public class GameProject_BP : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AudioCapture", "AudioAnalyzer", "AudioMixer" , "UMG","Slate","SlateCore", "OnlineSubsystem", "OnlineSubsystemSteam" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AudioCapture", "AudioAnalyzer", "AudioMixer", "Sockets", "Networking", "OnlineSubsystemSteam", "OnlineSubsystem" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "AudioCapture", "AudioAnalyzer", "AudioMixer" });
 
--- Server/GameProject_BP/Source/GameProject_BP/Private/SoundAnalyzerBase.cpp
+++
@@ -1,1 +0,0 @@
-#include "SoundAnalyzerBase.h"
