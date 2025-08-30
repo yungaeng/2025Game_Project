dc33009d579757f3ce421c92b65cd45850654785
Hyebin <112963362+Hbyien@users.noreply.github.com>
2025-08-30 18:57:47 +0900

    임무 ID, 인터페이스

---
+++ Client/GameProject_BP/Intermediate/Build/Win64/UnrealEditor/Inc/GameProject_BP/UHT/CPP_Screeming.generated.h
@@ -0,0 +1,75 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "CPP_Screeming.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+class AActor;
+class UAudioComponent;
+class UPrimitiveComponent;
+struct FHitResult;
+#ifdef GAMEPROJECT_BP_CPP_Screeming_generated_h
+#error "CPP_Screeming.generated.h already included, missing '#pragma once' in CPP_Screeming.h"
+#endif
+#define GAMEPROJECT_BP_CPP_Screeming_generated_h
+
+#define FID_Users_user_Desktop_2025Game_Project_Client_GameProject_BP_Source_GameProject_BP_Public_CPP_Screeming_h_19_SPARSE_DATA
+#define FID_Users_user_Desktop_2025Game_Project_Client_GameProject_BP_Source_GameProject_BP_Public_CPP_Screeming_h_19_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Users_user_Desktop_2025Game_Project_Client_GameProject_BP_Source_GameProject_BP_Public_CPP_Screeming_h_19_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Users_user_Desktop_2025Game_Project_Client_GameProject_BP_Source_GameProject_BP_Public_CPP_Screeming_h_19_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execOnBoxEndOverlap); \
+	DECLARE_FUNCTION(execOnBoxBeginOverlap); \
+	DECLARE_FUNCTION(execOnEnvelopeValue); \
+	DECLARE_FUNCTION(execOnSoundAnalyzerCleared);
+
+
+#define FID_Users_user_Desktop_2025Game_Project_Client_GameProject_BP_Source_GameProject_BP_Public_CPP_Screeming_h_19_ACCESSORS
+#define FID_Users_user_Desktop_2025Game_Project_Client_GameProject_BP_Source_GameProject_BP_Public_CPP_Screeming_h_19_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACPP_Screeming(); \
+	friend struct Z_Construct_UClass_ACPP_Screeming_Statics; \
+public: \
+	DECLARE_CLASS(ACPP_Screeming, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/GameProject_BP"), NO_API) \
+	DECLARE_SERIALIZER(ACPP_Screeming)
+
+
+#define FID_Users_user_Desktop_2025Game_Project_Client_GameProject_BP_Source_GameProject_BP_Public_CPP_Screeming_h_19_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACPP_Screeming(ACPP_Screeming&&); \
+	NO_API ACPP_Screeming(const ACPP_Screeming&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACPP_Screeming); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACPP_Screeming); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ACPP_Screeming) \
+	NO_API virtual ~ACPP_Screeming();
+
+
+#define FID_Users_user_Desktop_2025Game_Project_Client_GameProject_BP_Source_GameProject_BP_Public_CPP_Screeming_h_16_PROLOG
+#define FID_Users_user_Desktop_2025Game_Project_Client_GameProject_BP_Source_GameProject_BP_Public_CPP_Screeming_h_19_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_user_Desktop_2025Game_Project_Client_GameProject_BP_Source_GameProject_BP_Public_CPP_Screeming_h_19_SPARSE_DATA \
+	FID_Users_user_Desktop_2025Game_Project_Client_GameProject_BP_Source_GameProject_BP_Public_CPP_Screeming_h_19_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Users_user_Desktop_2025Game_Project_Client_GameProject_BP_Source_GameProject_BP_Public_CPP_Screeming_h_19_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Users_user_Desktop_2025Game_Project_Client_GameProject_BP_Source_GameProject_BP_Public_CPP_Screeming_h_19_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Users_user_Desktop_2025Game_Project_Client_GameProject_BP_Source_GameProject_BP_Public_CPP_Screeming_h_19_ACCESSORS \
+	FID_Users_user_Desktop_2025Game_Project_Client_GameProject_BP_Source_GameProject_BP_Public_CPP_Screeming_h_19_INCLASS_NO_PURE_DECLS \
+	FID_Users_user_Desktop_2025Game_Project_Client_GameProject_BP_Source_GameProject_BP_Public_CPP_Screeming_h_19_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> GAMEPROJECT_BP_API UClass* StaticClass<class ACPP_Screeming>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_user_Desktop_2025Game_Project_Client_GameProject_BP_Source_GameProject_BP_Public_CPP_Screeming_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Client/GameProject_BP/Intermediate/Build/Win64/UnrealEditor/Inc/GameProject_BP/UHT/GameProject_BP.init.gen.cpp
@@ -0,0 +1,35 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeGameProject_BP_init() {}
+	GAMEPROJECT_BP_API UFunction* Z_Construct_UDelegateFunction_GameProject_BP_OnPitchDetected__DelegateSignature();
+	GAMEPROJECT_BP_API UFunction* Z_Construct_UDelegateFunction_GameProject_BP_OnSoundAnalyzerClear__DelegateSignature();
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_GameProject_BP;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_GameProject_BP()
+	{
+		if (!Z_Registration_Info_UPackage__Script_GameProject_BP.OuterSingleton)
+		{
+			static UObject* (*const SingletonFuncArray[])() = {
+				(UObject* (*)())Z_Construct_UDelegateFunction_GameProject_BP_OnPitchDetected__DelegateSignature,
+				(UObject* (*)())Z_Construct_UDelegateFunction_GameProject_BP_OnSoundAnalyzerClear__DelegateSignature,
+			};
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/GameProject_BP",
+				SingletonFuncArray,
+				UE_ARRAY_COUNT(SingletonFuncArray),
+				PKG_CompiledIn | 0x00000000,
+				0x3FB55A44,
+				0xCC76B9AB,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_GameProject_BP.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_GameProject_BP.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_GameProject_BP(Z_Construct_UPackage__Script_GameProject_BP, TEXT("/Script/GameProject_BP"), Z_Registration_Info_UPackage__Script_GameProject_BP, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x3FB55A44, 0xCC76B9AB));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
